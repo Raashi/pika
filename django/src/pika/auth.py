03a82a724a1acc4483ebb5c9842a590e9faed914
Podkidyshev Ivan <ivan.podkidyshev@opendev.com>
2020-02-10 16:03:22 +0400

    implented user model

---
+++ django/src/pika/auth.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+
+class AdminAuthBackend:
+    def authenticate(self, request, username, password):
+        try:
+            user = UserModel.objects.get(username=username, is_admin=True)
+        except UserModel.DoesNotExist:
+            # According to django.contrib.auth.backends.ModelBackend
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a nonexistent user (#20760).
+            UserModel().set_password(password)
+        else:
+            if user.check_password(password):
+                return user
+
+    @staticmethod
+    def get_user(user_id):
+        try:
+            return UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
