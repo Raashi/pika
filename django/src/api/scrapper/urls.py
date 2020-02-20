fdfee2fbb1b50035e79a6802b0f88379923c058a
Podkidyshev Ivan <ivan.podkidyshev@opendev.com>
2020-02-20 10:15:02 +0400

    deleted slashes in urls

--- django/src/api/scrapper/urls.py
+++ django/src/api/scrapper/urls.py
@@ -6,13 +6,13 @@ app_name = 'scrapper'
 
 
 urlpatterns = [
-    path('login/', scrapper.LoginView.as_view(), name='login'),
+    path('login', scrapper.LoginView.as_view(), name='login'),
 
-    path('countries/', scrapper.base.CountriesUploadView.as_view(), name='countries'),
-    path('languages/', scrapper.base.LanguagesUploadView.as_view(), name='languages'),
-    path('jobs/', scrapper.base.JobsUploadView.as_view(), name='jobs'),
-    path('bases/', scrapper.base.BasesUploadView.as_view(), name='bases'),
-    path('persons/', scrapper.person.PersonsUploadView.as_view(), name='persons'),
-    path('movies/', scrapper.movie.MoviesUploadView.as_view(), name='movies'),
-    path('movies/relations/', scrapper.movie.MoviesRelationsUploadView.as_view(), name='movies-relations')
+    path('countries', scrapper.base.CountriesUploadView.as_view(), name='countries'),
+    path('languages', scrapper.base.LanguagesUploadView.as_view(), name='languages'),
+    path('jobs', scrapper.base.JobsUploadView.as_view(), name='jobs'),
+    path('bases', scrapper.base.BasesUploadView.as_view(), name='bases'),
+    path('persons', scrapper.person.PersonsUploadView.as_view(), name='persons'),
+    path('movies', scrapper.movie.MoviesUploadView.as_view(), name='movies'),
+    path('movies/relations', scrapper.movie.MoviesRelationsUploadView.as_view(), name='movies-relations')
 ]
